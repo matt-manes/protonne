61e23b7d14599e6165a0aa03f1d99a29ef04ff60
matt manes <mattmanes@pm.me>
2023-07-05 17:55:57 -0500

    feat: add connection functions

--- src/protonne/proton.py
+++ src/protonne/proton.py
@@ -92,6 +92,66 @@ class Proton(Morbin):
     def logout(self) -> Output:
         return self.proton("logout")
 
+    def connect(
+        self,
+        server: str = "",
+        protocol: str = "",
+        country_code: str = "",
+        fastest: bool = False,
+        random: bool = False,
+        secure_core: bool = False,
+        p2p: bool = False,
+        tor: bool = False,
+    ) -> Output:
+        """
+        #### :params:
+
+        `server`: Connect to this specific server (e.g. `CH#4`, `CH-US-1`, `HK5-Tor`).
+
+        `protocol`: Connect using this protocol (`tcp` or `udp`).
+
+        `country_code`: Connect to a server in this country (`SE`, `PT`, `BR`, `AR`, etc.).
+
+        `fastest`: Connect to the fastest server.
+
+        `random`: Connect to a random server.
+
+        `secure_core`: Connect to the fastest Secure-Core server.
+
+        `p2p`: Connect to the fastest P2P server.
+
+        `tor`: Connect to the fastest Tor server.
+
+        Supplying an argument for `server` will override other parameters except `protocol`.
+
+        If supplying one of the `bool` params (`fastest`, `random`, `secure_core`, `p2p`, `tor`), supply only one.
+        They will be checked in the order they appear in the function signature and the first that is `True` will be used.
+        """
+        args = ""
+        if server:
+            args = server
+        elif fastest:
+            args = "--fastest"
+        elif random:
+            args = "--random"
+        elif secure_core:
+            args = "--sc"
+        elif p2p:
+            args = "--p2p"
+        elif tor:
+            args = "--tor"
+        if country_code and not server:
+            args += f" --cc {country_code}"
+        if protocol:
+            args += f" --protocol {protocol}"
+        return self.proton(f"connect {args}")
+
+    def disconnect(self) -> Output:
+        return self.proton("disconnect")
+
+    def reconnect(self) -> Output:
+        return self.proton("reconnect")
+
     # Seat |=========================== Convenience ===========================|
 
     @property
